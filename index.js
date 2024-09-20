38f25692c02c2ee83e95c43474ae530d432771ce
leticiaxavier <50848766+LeticiaXavier@users.noreply.github.com>
2024-09-20 03:19:18 -0300

    Organizando o Menu

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
 const fs  = require('fs');
 const crypto = require('crypto');
 const inquirer = require('inquirer');
+const { select } = require('@inquirer/prompts');
+
+// Iniciando a aplicação
+
+const start = async () => {
+    //Carrehar sennhas
+
+    while (true) {
+        const opcao = await select({
+            message: 'Menu de Gerenciameno de Senhas >>',
+
+            choices: [{
+                name: 'Gerar uma nova senha',
+                value: 'Senha'
+            },
+            {
+                name: 'Visualizar senhas',
+                value: 'Visualizar'  
+            },
+            {
+                name: 'Deletar senha',
+                value: 'Deletar'
+            },
+            {
+                name: 'Sair',
+                value: 'Sair'
+            },
+        ],
+        })
+
+        switch (opcao){
+
+            case "Gerar nova senha":
+                console.log("Nova senha");
+                break
+
+            case "Visualizar Senhas":
+                break
+
+            case "Deletar senha":
+                break
+            case "Sair":
+                console.log("Ate a proxima!")
+                return    
+        }
+    }
+}
+start()
